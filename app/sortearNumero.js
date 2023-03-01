154adc1cc35cd5f29d8c67b567abaedb9a7c3830
julucca <lucca.editora@gmail.com>
2023-03-01 13:49:00 -0300

    feat: create random number function

---
+++ app/sortearNumero.js
@@ -0,0 +1,15 @@
+const menorValor = 1;
+const maiorValor = 100;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * ((maiorValor + 1) - menorValor) + (menorValor));    
+}
+
+console.log('Número Secreto: ' + numeroSecreto);
+
+const elementoMenorValor = document.querySelector("#menor-valor");
+const elementoMaiorValor = document.querySelector("#maior-valor");
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
