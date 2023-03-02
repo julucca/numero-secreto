1ad97a4d73002398856b9b8f0a8d72dfc818a0b1
julucca <lucca.editora@gmail.com>
2023-03-02 14:40:44 -0300

    feat: create number validation

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,26 +1,25 @@
-// Busca div no DOM
 const elementoChute = document.querySelector('#chute');
 
-// Ativa microfone no navegador
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
 recognition.start();
 
-// Atribui escutador de evento ao recognition
 recognition.addEventListener('result', onSpeak);
 
-// Capta fala do usuário
 function onSpeak(e) {
     const chute = e.results[0][0].transcript 
-    exibeChuteNaTela(chute);    
+    exibeChuteNaTela(chute);
+    verificaSeOChutePossuiUmValorValido(chute);    
 }
 
-// Exibe na tela
 function exibeChuteNaTela(chute) { 
     elementoChute.innerHTML = `
         <div>Você disse</div>
         <span class="box">${chute}</span>
     `
 }
+
+// manter o reconhecimento de voz ativo
+recognition.addEventListener('end', () => recognition.start());
